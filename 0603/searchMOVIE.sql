35aa0006a0176c4f640d5f2b4723baf3388520f3
jcry <gomdorij@naver.com>
2021-06-03 16:49:41 +0900

    주석추가

--- 0603/searchMOVIE.sql
+++ 0603/searchMOVIE.sql
@@ -1,3 +1,4 @@
+-- movie table 제작 (영화제목(PK), 개봉일, 평점, 관객 수, 상영 시간, 장르 ID)
 create table movie (movie_id varchar2(20),
                     release_date date,
                     rating int,
@@ -6,14 +7,16 @@ create table movie (movie_id varchar2(20),
                     genre_id int,
                     primary key(movie_id));
 
+-- actor table 제작 (배우 이름(PK), 출연 영화(FK), 역할, 소속사, 성별)
 create table actor (actor_id varchar2(20),
                     movie_id varchar2(20),
                     character varchar2(20),
                     agency varchar2(20),
-                    gender char(1) constraint actor_gender check (gender in ('F','M')),
+                    gender char(1) constraint actor_gender check (gender in ('F','M')), -- F 혹은 M만 적을 수 있도록 제한
                     primary key(actor_id),
                     foreign key(movie_id) references movie(movie_id));    
 
+-- director table 제작 (감독 이름(PK), 제작 영화(FK), 대표작, 나이)
 create table director (director_id varchar2(20),
                         movie_id varchar2(20),
                         popular varchar2(20),
@@ -21,13 +24,19 @@ create table director (director_id varchar2(20),
                         primary key(director_id),
                         foreign key(movie_id) references movie(movie_id));
 
+-- genre table 제작 (1:드라마 / 2:SF / 3:멜로 / 4:코미디)
+create table genre (genre_id int,
+                    genre_name varchar2(20),
+                    primary key(genre_id));
+
+-- movie 세부사항 추가
 insert into movie values('jurassic park',to_date('1991-06-11','yyyy-mm-dd'),9.32,555,132,2);
 insert into movie values('Titanic',to_date('1998-02-20','yyyy-mm-dd'),9.88,197,194,3);
 INSERT INTO movie VALUES('기생충', to_date('2019-05-30','yyyy-mm-dd'), 9.07, 1030, 132, 1);
 insert into movie values ('Cruella',to_date('2021-05-26','yyyy-mm-dd'),8.7,38,133,4);
 insert into movie values('미나리', to_date('2021-03-03','yyyy-mm-dd'), 8.31, 110, 115, 1);
 
-
+-- actor 세부사항 추가
 insert into actor values('Sam Neill','jurassic park','DR.allen grant','Curtis Brown', 'M');
 insert into actor values('Laura Dern','jurassic park','Ellie Sattler','IMDbPro', 'F');
 insert into actor values('Jeff Goldblum','jurassic park','Ian Malcolm','Elevate Artist', 'M');
@@ -49,9 +58,16 @@ insert into actor values('한예리', '미나리', '모니카', '사람엔터', 
 insert into actor values('윤여정', '미나리', '순자', '후크엔터', 'F');
 insert into actor values('앨런김', '미나리', '데이빗', 'CAA', 'M');
 
-
+-- director 세부사항 추가
 insert into director values('Steven Spielberg','jurassic park','ET',74);
 insert into director values('James Cameron','Titanic','Avatar',66);
 INSERT INTO director VALUES('봉준호','기생충','괴물', 53);
 insert into director values ('Craig Gillespie', 'Cruella', 'Lars and Real Girl', 53);
 insert into director values('정이삭','미나리','괴물',45);
+
+-- genre 구분 추가
+insert into genre values(1,'드라마');
+insert into genre values(2,'SF');
+insert into genre values(3,'Melo/Romance');
+insert into genre values(4,'Comedy');
+
